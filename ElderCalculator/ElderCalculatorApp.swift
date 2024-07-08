186c4a68a8976f13d4287fa8e513a76d494e5592
Rangga <rangga.yudhistira@ui.ac.id>
2024-07-08 11:52:22 +0700

    Initial Commit

---
+++ ElderCalculator/ElderCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElderCalculatorApp.swift
+//  ElderCalculator
+//
+//  Created by Rangga Yudhistira Brata on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ElderCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
