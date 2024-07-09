e69c6fa5d0aa190661184f1603e291d36f55a74e
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-07-09 09:42:59 +0700

    font extension

--- ElderCalculator/ContentView.swift
+++ ElderCalculator/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.sf(size: 16, weight: .heavy))
         }
         .padding()
     }
