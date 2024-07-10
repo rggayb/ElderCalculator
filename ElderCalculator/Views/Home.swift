1afbd3227b2a13ce13d011a88a42c080f6ccebb8
rayhanjamesc <rayhanjamesc@gmail.com>
2024-07-10 13:36:33 +0700

    Changes: - Added CategoryInput for people to choose the category of the grocery item they've added to the cart - Added QuantityInput for people to adjust the quantity of items they want to add to the shopping cart - Implemented CategoryInput and QuantityInput models to the Home view

--- ElderCalculator/Views/Home.swift
+++ ElderCalculator/Views/Home.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct Home: View {
     @State private var price = ""
     @State private var discount = ""
+    @State private var quantity = ""
+    @State private var selectedCategory = ""
     @State private var total = 0.0
     @State var isTaxIncluded = false
     
     let taxPercentage = 0.11
+    let categories = ["Fruit", "Vegetable", "Dairy", "Meat"]
     
     var body: some View {
         ZStack {
@@ -25,8 +28,14 @@ struct Home: View {
                 //Total spendings
                 TotalSpendings(total: total)
                 
+                //Category input
+                CategoryInput(selectedCategory: $selectedCategory, categories: categories)
+                
                 //Price input
                 PriceInput(price: $price)
+                
+                //Quantity input
+                QuantityInput(quantity: $quantity)
                 
                 //Discount input
                 DiscountInput(discount: $discount)
@@ -50,7 +59,8 @@ struct Home: View {
     func calculateTotal() {
         let priceValue = Double(price) ?? 0.0
         let discountValue = (Double(discount) ?? 0.0) / 100.0
-        var totalValue = priceValue - (priceValue * discountValue)
+        let quantityValue = Double(quantity) ?? 1.0
+        var totalValue = (priceValue - (priceValue * discountValue)) * quantityValue
         
         //Includes tax if toggle is on
         if isTaxIncluded {
