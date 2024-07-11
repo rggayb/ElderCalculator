c6c3fc87a198bb6ee7a7cdfde53842a728299b87
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-07-11 17:28:04 +0700

    + layout add new trip

--- ElderCalculator/View/AddNewTripView.swift
+++ ElderCalculator/View/AddNewTripView.swift
@@ -17,15 +17,71 @@ struct AddNewTripView: View {
     @State var tax: String = ""
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Store name", text: $storeName)
-                TextField("budget", text: $budget)
-                    .keyboardType(.numberPad)
-                TextField("tax", text: $tax)
-                    .keyboardType(.numberPad)
+        VStack(spacing: 48){
+            // headline
+            VStack(spacing: 8){
+                Text("Get ready to shop")
+                    .font(.system(size: 32, weight: .bold))
+                Text("Input the store name and\nstate your budget.")
+                    .font(.system(size: 20, weight: .regular))
+                    .multilineTextAlignment(.center)
             }
             
+            // all below headline
+            VStack(spacing:16){
+                // store and budget
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(height: 88)
+                    .foregroundColor(Color(hex: "F2F2F7"))
+                    .overlay{
+                        VStack{
+                            HStack{
+                                Text("Store\t\t")
+                                TextField("Name", text: $storeName)
+                                    .disableAutocorrection(true)
+                            }
+                            Divider()
+                            HStack{
+                                Text("Budget\t\t")
+                                HStack(spacing:4){
+                                    Text("Rp")
+                                        .foregroundColor(Color(hex:"BCBCC0"))
+                                    TextField("", text: $budget)
+                                        .keyboardType(.numberPad)
+                                }
+                            }
+                        }
+                        .padding()
+                }
+                
+                // specify tax
+                VStack {
+                    Section(header:
+                        HStack {
+                            Text("Specify Tax Percentage for this Store")
+                                .font(.sf(size: 16, weight: .semibold))
+                            Spacer()
+                        }
+                    ){
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(height: 44)
+                            .foregroundColor(Color(hex: "F2F2F7"))
+                            .overlay{
+                                HStack{
+                                    Text("Tax")
+                                    HStack {
+                                        TextField("11", text: $tax)
+                                            .keyboardType(.numberPad)
+                                            .multilineTextAlignment(.trailing)
+                                        Text("% VAT")
+                                    }
+                                }
+                                .padding()
+                            }
+                    }
+                }
+            }
+            Spacer()
             Button (action: {
                 viewModel.addTrip(
                     storeName: storeName,
@@ -34,16 +90,18 @@ struct AddNewTripView: View {
                 )
                 dismiss()
             }) {
-                Text("Save")
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 16)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: .infinity, height: 50)
+                    .overlay{
+                        Text("Save")
+                            .foregroundColor(.white)
+                            .padding()
+                    }
             }
-            
         }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .frame(width: UIScreen.main.bounds.width-48)
+        .padding(.vertical, 48)
     }
 }
 
