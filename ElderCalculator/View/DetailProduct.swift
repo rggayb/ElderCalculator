cd6bb5726532088b5b043a2e41ad33f38779d3b6
Michelle C <micheelechau@gmail.com>
2024-07-16 08:59:08 +0700

    calculate total monthly functionality, product detail view and logic

---
+++ ElderCalculator/View/DetailProduct.swift
@@ -0,0 +1,8 @@
+//
+//  DetailProduct.swift
+//  ElderCalculator
+//
+//  Created by Michelle Chau on 16/07/24.
+//
+
+import Foundation
---
+++ ElderCalculator/ViewModel/ProductDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailViewModel.swift
+//  ElderCalculator
+//
+//  Created by Michelle Chau on 16/07/24.
+//
+
+import Foundation
